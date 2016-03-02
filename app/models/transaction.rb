3848cd98235a3183cdb1584436ce7def962a10ee
Steven Cooper <stevengcooper76@gmail.com>
2016-03-02 17:10:10 -0500

    Transactions can count number of processes.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,8 @@ class Transaction < ActiveRecord::Base
   def self.display_balance
     self.all.reduce(0.0) {|n,m| n + m.amount}
   end
+
+  def self.count_processes
+    self.distinct.count('date')
+  end
 end
--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -5,4 +5,8 @@ class TransactionTest < ActiveSupport::TestCase
   test "transaction can display balance" do
     assert_equal 20.0, Transaction.display_balance
   end
+
+  test "number of transactions can be counted" do
+    assert_equal 2, Transaction.count_processes
+  end
 end
